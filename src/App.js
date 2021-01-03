c56286dc37d787d4d9eff485039883e38da692b0
Chann <princechanns@gmail.com>
2021-01-03 21:33:28 +0300

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import Navabar from './components/navbar'
 
 function App() {
   return (
-    <h1>porfolio project</h1>
+    <Navabar />
   );
 }
 
